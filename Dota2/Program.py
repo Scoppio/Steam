274c7db36e50431b1fb62f95106382b8a3f5b344
TeoCalvo <teocalvo2@gmail.com>
2016-01-04 17:05:55 -0200

    Ordem no Caos

--- Dota2/Program.py
+++ Dota2/Program.py
@@ -7,6 +7,6 @@ import time
 chave = input(" Entre com a chave do desenvolvedor: ")
 int_tempo = int(input("\n Defina um intervalo de tempo para coleta (em segundos): "))
 
-minha_coleta = info_partidas(chave)
+minha_coleta = infos_partidas(chave)
 minha_coleta.coleta_recursiva(int_tempo)
 
