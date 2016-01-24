33075efd686247015be72bb383cf8b10bc100ae5
TeoCalvo <teocalvo2@gmail.com>
2016-01-24 04:22:22 -0200

    Ordem no Caos!

--- Dota2/funcoes.py
+++ Dota2/funcoes.py
@@ -7,7 +7,7 @@ def dormindo(tempo, i):
         segundos = (tempo - t) % 60
         os.system("clear")
         print("\n " + str(i) + "a Coleta realizada e salva com sucesso!!\n")
-        print("\n A próxima coletá será em : "+str(minutos)+ " minutos e " + str(segundos) + " segundos" )
+        print("  A próxima coletá será em : "+str(minutos)+ " minutos e " + str(segundos) + " segundos" )
         time.sleep(1)
     os.system("clear")
     return None
