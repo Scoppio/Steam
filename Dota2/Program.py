c1a75aea516074486930ceff8c2add75d5c66271
TeoCalvo <teocalvo2@gmail.com>
2016-02-04 03:28:55 -0200

    Ordem no Caos

--- Dota2/Program.py
+++ Dota2/Program.py
@@ -35,7 +35,7 @@ def coleta_recursiva(chave, endereco = False, tempo=0, nlinhas=10000):
 #### CHAVE DA STEAM PARA ACESSO ####
 chave = input(" Entre com a chave do desenvolvedor: ")
 tempo = int(input("\n Defina um intervalo de tempo para coleta (em segundos): "))
-nlinhas = int(input("\n Entre com o número maximo de linahs: "))
+nlinhas = int(input("\n Entre com o número maximo de linhas: "))
 salvar = input("\n Deseja salvar a base de dados?[S/n]: ").upper()
 
 
