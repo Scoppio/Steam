c7a9062ad9724a02ffb654673efcf4adf828fc0d
TeoCalvo <teocalvo2@gmail.com>
2016-02-04 05:38:03 -0200

    Ordem no Caos

--- Dota2/Program.py
+++ Dota2/Program.py
@@ -19,10 +19,11 @@ def coleta_recursiva(chave, endereco = False, tempo=0, nlinhas=10000):
         dados = dados.append( minha_coleta.faz_coleta(ids=minha_coleta.pega_ids()) )
         dados = dados.drop_duplicates()
 
+        end_salvar = endereco[:-4] + "_" + str(j) +".csv"
+        print("Os dados estão sendo salvos no arquivo:" + end_salvar)
+        dados.to_csv(end_salvar)
+
         if dados.shape[0] > nlinhas:
-            end_salvar = endereco[:-4] + "_" + str(j) +".csv"
-            print("Os dados estão sendo salvos no arquivo:" + end_salvar)
-            dados.to_csv(end_salvar)
             del dados
             dados = pd.DataFrame(columns=minha_coleta.colunas)
             j+=1
