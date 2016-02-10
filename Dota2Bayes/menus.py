1aad194b405ab1ed62c2f862bb78a671d5dac3aa
TeoCalvo <teocalo2@gmail.com>
2016-02-10 13:58:04 -0200

    update de Férias

--- Dota2Bayes/menus.py
+++ Dota2Bayes/menus.py
@@ -22,39 +22,20 @@ def print_dota2bayes(tamanho=60):
 	print()
 	return None
 
-def selecao(tamanho = 60):
+def mostra_selecao(tamanho = 60):
 	system("clear")
 	print()
 	print(" MENU DE OPÇÕES ".center(tamanho))
 	print("+---------------------------+-----+".center(tamanho))
-	print("| Visualizar Base de Herois |  1  |".center(tamanho))
+	print("|     Visualizar Herois     |  1  |".center(tamanho))
 	print("|    Previsão de Partida    |  2  |".center(tamanho))
 	print("|            Sair           |  3  |".center(tamanho))
 	print("+---------------------------+-----+".center(tamanho))
-	
-def mostra_herois():
 
-	print(herois[ ["id", "heroi"] ])
-	input(" Aperte qualquer tecla para retornar")
-
-def inicio(tamanho=60):
-
-	system("clear")
-	print("\n" + "Seja bem vindo".center(tamanho)+"\n")
-	print_dota2bayes(tamanho)
-	input("Aperte qualquer tecla para iniciar")
-	selecao()
+def faz_selecao(tamanho = 60):
+	mostra_selecao(tamanho)
 	escolha = input("\nEntre com o valor da opção desejada: ")
-	if escolha == "1":
-		mostra_herois()
-
-
-
-def linha_heroi(heroi, i_heroi):
-
-	linha = " | " + ("HEROI " + str(i_heroi) + ": ").ljust(10) + heroi.center(19) + " |"
-	linha = linha.center(len(linha))
-	return linha
+	return escolha
 
 def todos_herois(herois):
 	tamanho_heroi = max([len(h) for h in herois])
@@ -64,6 +45,23 @@ def todos_herois(herois):
 		linhas.append( linha_heroi(h, i) )
 		i+=1
 	return linhas
+
+def base_herois(base_herois):
+	print(" +---------------------------+")
+	print(" | " + "ID".center(3) + " | " + "HEROI".center(19) + " |")
+	print(" +---------------------------+")
+
+	for h in range(base_herois.shape[0]):
+		print(" | " + str(herois.loc[h]["id"]).rjust(3) + " | " + str(herois.loc[h]["heroi"]).center(19) + " |" )
+	
+	print(" +---------------------------+")
+	input(" Aperte qualquer tecla para retornar")
+
+def linha_heroi(heroi, i_heroi):
+
+	linha = " | " + ("HEROI " + str(i_heroi) + ": ").ljust(10) + heroi.center(19) + " |"
+	linha = linha.center(len(linha))
+	return linha
 
 def mostra_time(time, herois):
 	
@@ -81,6 +79,22 @@ def mostra_time(time, herois):
 			print( colored(l, "red") )
 
 	return None
+
+def inicio(tamanho=60):
+
+	system("clear")
+	print("\n" + "Seja bem vindo".center(tamanho)+"\n")
+	print_dota2bayes(tamanho)
+	input("Aperte qualquer tecla para iniciar")
+	while True:
+		escolha = faz_selecao()
+		if escolha == "1":
+			base_herois(herois)
+		if escolha == "2":
+			print("Função não esta pronta!")
+			input(" Aperte qualquer tecla para retornar")
+		if escolha == "3":
+			break
 
 inicio()
 mostra_time("RADIANT", ["Tedoro Calvo", "Emanuel Alvares", "Lara Calvo"])
