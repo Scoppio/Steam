28e5212ad18eb428e95476c163ea68c87ddc3113
TeoCalvo <teocalvo2@gmail.com>
2016-02-16 01:43:24 -0200

    update de Férias

--- Dota2Take/naiveBayes.py
+++ Dota2Take/naiveBayes.py
@@ -17,28 +17,43 @@ class naiveBayes():
 		fdp = ( 1 / math.sqrt( 2 * math.pi * variancia ) ) * ( math.e ** ( (-1/2) * ( (x - media) ** 2 ) / variancia 	) )
 		return fdp
 
-	def estimar_media(self, X, Y):
+	def estimar(self, X, Y, estat):
 
+		classes = Y.unique()
+		classes.sort()
 		estimativa = pd.DataFrame(columns = X)
-		estimativa = estimativa.append(X.mean(), ignore_index=True)
-		estimativa = estimativa.append(X[Y==True].mean(), ignore_index=True)
-		estimativa = estimativa.append(X[Y==False].mean(), ignore_index=True)
-		estimativa.index = ["geral", True, False]
-		return estimativa
 
-	def estimar_variancia( self, X, Y):
+		if estat == "media":
+			estimativa = estimativa.append(X.mean(), ignore_index=True)
+			for c in classes:
+				estimativa = estimativa.append(X[Y==c].mean(), ignore_index=True)
+			estimativa.index = ["geral"] + list(classes)
+			return estimativa
 
-		estimativa = pd.DataFrame(columns = X)
-		estimativa = estimativaa.ppend(X.var(), ignore_index=True)
-		estimativa = estimativa.append(X[Y==True].var(), ignore_index=True)
-		estimativa = estimativa.append(X[Y==False].var(), ignore_index=True)
-		estimativa.index = ["geral", True, False]
-		return estimativa
+		elif estat == "variancia":
+			estimativa = estimativa.append(X.var(), ignore_index=True)
+			for c in classes:
+				estimativa = estimativa.append(X[Y==c].var(), ignore_index=True)
+			estimativa.index = ["geral"] + list(classes)
+			return estimativa
+
+	def faz_X_N( linha ):
+		
+		hero_1 = list( linha[linha == 1].index )
+		if 'modo_jogo' in hero_1:
+			hero_1.remove('modo_jogo')
+		X_N = []
+		for h in hero_1:
+			X_N.append( h + "_xp_per_min" )
+			X_N.append( h + "_gold_per_min" )
+
+		return linha[X_N]
+
 
 	def ajuste(self, dados, X_B, X_N, Y):
 
-		medias = estimar_media( dados[X_B + X_N] )
-		variancias = estimar_variancia( dados[ X_N ] )
+		medias = estimar( dados[X_B + X_N], Y, "media" )
+		variancias = estimar( dados[ X_N ], Y, "variancia" )
 
 		self.parametros = { "Media":medias  , "Variancia": variancias}
 		return None
